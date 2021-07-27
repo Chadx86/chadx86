2a02516a72c0cb3becbd13ce1cdbd857b872be38
Marcelo Rios <45809332+PedroElFrijol@users.noreply.github.com>
2021-07-26 23:50:33 -0700

    Update main_uefi.c
    still working on inputting and outputting text with the test protocols

--- src/main_uefi.c
+++ src/main_uefi.c
@@ -1,4 +1,5 @@
 //Alot of the comments are from the UEFI Specs 2.9, some comments are added in
+//UEFI Specs is the reference I used for the code (PedroElFrijol)
 
 #define EFI_SUCCESS 0; //Also similar to Return 0, you could use that if you'd like
 
@@ -7,7 +8,11 @@ typedef unsigned short int uint16_t; //2 byte unsigned integer
 
 typedef unsigned int uint32_t; //4 byte unsigned integer
 
-typedef uint32_t EFI_STATUS;
+typedef signed long long INTN; //Signed value of native width
+
+typedef unsigned long long UINTN; //Unsigned value of native width
+
+typedef void *EFI_HANDLE; //this is here as a pointer
 
 //We are using and unsigned short int since that is equal to 16 bits
 
@@ -35,16 +40,43 @@ typedef unsigned long long UINT64; //8-byte unsigned value
 
 //It is set to long long instead of int because it is 8 bytes (64 bits) and it is larger
 
+tyoedef UINT64 EFI_STATUS;
+
 typedef void *EFI_HANDLE; //A collection of related interfaces
 
 //We are using void * since that is the type the uefi specs said it should be
 
-typedef unsigned char BOOLEAN; //Logical Boolean. 1-byte value containing a 0 for FALSE or a 1 for TRUE. Other values are undefined.
+typedef unsigned char BOOLEAN; //Logical Boolean. 1-byte value containing a 0 for FALSE or a 1 for TRUE. (just a regular boolean)
 
-typedef uint16_t CHAR16; //2-byte Character. Unless otherwise specified all characters and strings are stored in the UCS-2 encoding format as defined by Unicode 2.1 and ISO/IEC 10646 standards.
+typedef uint16_t CHAR16; //2-byte Character
 
-CHAR16 *Str; //Str is short for string
+struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL; //Protocol interfaces for devices that support console style text displaying.
 
-Str = L"Welcome to the Chadx86 bootloader! \n";
+typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL //Output text
+{
+    EFI_TEXT_RESET Reset;
+    EFI_TEXT_STRING OutputString;
+} EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
 
-//We would call return 0 so it exits the efi application, I will add this soon
+typedef struct EFI_SYSTEM_TABLE
+{
+    EFI_TABLE_HEADER hrd;
+    CHAR16 *FirmwareVendor;
+    UINT32 FirmwareVersion;
+    EFI_HANDLE ConsoleInHandle;
+    EFI_SIMPLE_TEXT_INPUT_PROTOCOL *ConIn; //EFI_SIMPLE_TEXT_INPUT_PROTOCOL is similar to ConIn in c++
+    EFI_HANDLE ConsoleOutHandle;
+    EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *ConOut; //EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL is similar to ConOut in c++
+  
+} EFI_SYSTEM_TABLE ;
+
+typedef EFI_STATUS uefi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+  
+  CHAR16 *Str; //Str is short for string
+  
+  Str = L"Welcome to the Chadx86 bootloader! \n";
+  
+  return EFI_SUCCESS; // exits UEFI application (similar to return 0)
+  
+}
